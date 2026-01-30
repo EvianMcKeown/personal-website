36ddca46d7b21bac8f4ad4fdd526e57a561b49c5
EvianMcKeown <evian.mck@gmail.com>
2026-01-30 07:55:40 +0200

    safari ios bar fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,5 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/personal-website/", // important for GitHub Pages
+  base: "/personal-website/", // for GitHub Pages
 });
